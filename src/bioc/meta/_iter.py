823f79729052b3d3643bfd1964d3ff6dbd93db63
hernani <hernani@clementine.vecirex.net>
2013-09-04 17:31:34 +0200

    Remove unused code

--- src/bioc/meta/_iter.py
+++ src/bioc/meta/_iter.py
@@ -2,14 +2,5 @@ __all__ = []
 
 class _MetaIter:
 
-    '''
-    def next(self):
-        for elem in self._iterdata: # Requires self._iterdata to exist
-            try:
-                yield elem
-            except StopIteration:
-                break
-    '''
-
     def __iter__(self):
         return self._iterdata().__iter__()
